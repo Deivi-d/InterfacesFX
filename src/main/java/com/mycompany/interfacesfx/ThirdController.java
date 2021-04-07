3cbdc0933410a6765811d14fcaf5fde8c905ede3
david <david@DESKTOP-N4I9J7L.home>
2021-04-07 18:46:14 +0200

    Version 1 interfaces

---
+++ src/main/java/com/mycompany/interfacesfx/ThirdController.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.interfacesfx;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class ThirdController {
+
+    @FXML
+    private void switchToThird() throws IOException {
+        App.setRoot("primary");
+    }
+}  
+    
+
